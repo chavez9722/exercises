6efbc56c2df7ad044a0b234dc65f8aeefefd6427
José Gustavo Chávez Conde <gustavo.chavez@vmasideas.com>
2023-05-07 20:37:26 -0600

    strings exercises

--- strings-1/js/exercise-4.js
+++ strings-1/js/exercise-4.js
@@ -2,7 +2,7 @@ const stringRepeat = (str = "Empty sring", num = 1) => {
     if (typeof str != "string" || str == false || typeof num != "number") {
         return "Error";
     }
-    return str.repeat(num);
+    str.repeat(num);
 }
 
 console.log(stringRepeat("Hola Mundo", 3));
